2feada2a1c68f79479fdde597b0e3ef12aa98b54
Aniruddh Arora <aniruddharora2001@gmail.com>
2024-12-08 16:20:48 -0500

    added evaluation for svm

--- models/preprocessing.py
+++ models/preprocessing.py
@@ -4,26 +4,39 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-def preprocessing(percentage):
+def preprocessing(percentage, kfold):
     file_path='../data/diabetes_binary_health_indicators_BRFSS2015.csv'
     # Load the dataset
     data = pd.read_csv(file_path)
     data = shuffle(data)
 
-    data = data[:int(len(data)*percentage)] # select the number of samples you want to use
+    data = data[:int(len(data)*0.0001)] # select the number of samples you want to use
 
     # drop features that have nan
     data = data.dropna()
 
     # Separate target and features
     target = 'Diabetes_binary'
-    X = data.drop(columns=[target])
+    X = data.drop(columns=[target, 'Stroke'])
     y = data[target]
 
-    # Normalize numerical features using Min-Max Scaling
+    # # correlation analysis commented out for now since still working on it
+    correlations = X.corrwith(y)
+    selected_features = correlations[correlations.abs() > 0.1].index
+    print("Selected features based on correlation:", selected_features.tolist())
+    # X = X[selected_features]
+
     numerical_columns = ['BMI', 'MentHlth', 'PhysHlth', 'Age', 'Income' , 'Education', 'GenHlth' ] 
+    existing_num_cols = []
+    
+    for feature in numerical_columns:
+        if feature in numerical_columns :
+            existing_num_cols.append(feature)
+    # Normalize numerical features using Min-Max Scaling
     scaler = MinMaxScaler()
-    X[numerical_columns] = scaler.fit_transform(X[numerical_columns])
+    X[existing_num_cols] = scaler.fit_transform(X[existing_num_cols])
+
+    # X = data.drop(columns=['Income'])
 
     
     def balance_classes(X, y):
@@ -57,9 +70,12 @@ def preprocessing(percentage):
 
     X_balanced, y_balanced = balance_classes(X, y)
 
-    # Split the data into training and testing sets (80/20 split)
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_balanced, y_balanced, test_size=0.2, random_state=42
-    )
+    if not kfold:
+        # Split the data into training and testing sets (80/20 split)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_balanced, y_balanced, test_size=0.2, random_state=42
+        )
+        return X_train, X_test, y_train, y_test
 
-    return X_train, X_test, y_train, y_test
+    # if for k fold, we want to return all the data
+    return X_balanced, y_balanced
